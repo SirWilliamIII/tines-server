889b5481a160a454597023ac9966a871b7405081
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-04-15 13:03:19 -0500

    updated db users

--- server.js
+++ server.js
@@ -2,37 +2,33 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-
-app.use(express.json());
-
-
 const port = 3000;
 
-// Create a MySQL connection pool
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '4unIver$e2',
+    user: 'tines_user',
+    password: '!tines123Pw',
     database: 'tines',
 });
 
-// Connect to the MySQL server
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL server');
 });
 
-// Define a route to handle the POST request containing JSON data
+
+app.use(express.json());
+
 app.post('/data', (req, res) => {
     const { name, description } = req.body;
     const data = { name, description };
 
     const query = connection.query('INSERT INTO things SET ?', data, (err, result) => {
         if (err) throw err;
-        console.log('Data inserted into MySQL table');
-        res.send('Data inserted into MySQL table');
+        console.log('Success');
+        res.send('Data inserted into table');
     });
-
+    
     console.log(query.sql);
 });
 
